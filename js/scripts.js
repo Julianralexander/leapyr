8b4ec019050d462118f803faffefead9ff1c4700
Julian Alexander <julian52795@gmail.com>
2017-11-06 18:38:42 -0800

    Added first bit of example code from epidocus (before refactoring)

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+//B:E
+var leapYear = function(year) {
+  if (year % 100 === 0) {
+    return false;
+  } else if (year % 4 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+
+//U:I
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
+});
