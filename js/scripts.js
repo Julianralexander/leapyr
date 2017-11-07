dc546f2d7dcb3020eb9e3e1da37369bbd6cddfa0
Julian Alexander <julian52795@gmail.com>
2017-11-06 18:56:24 -0800

    Final epicodus version of the code, that works, but has minimal styling etc

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,27 @@
 //B:E
 var leapYear = function(year) {
-  if (year % 100 === 0) {
-    return false;
-  } else if (year % 4 === 0) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
     return true;
   } else {
     return false;
   }
 };
 
-
 //U:I
 $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
-
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
-    $("#result").text(result);
+
+    $(".year").text(year);
+
+    if (!result) {                 // same as writing if (result === false)
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
   });
 });
